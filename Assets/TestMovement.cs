5a768e52cb62ab521ae876d351e942f89d55e9fb
Haven <leoshelleyyellow8@gmail.com>
2024-03-15 14:59:13 +0000

    Big Development Time

--- Assets/TestMovement.cs
+++ Assets/TestMovement.cs
@@ -7,13 +7,26 @@ using UnityEngine.UIElements;
 public class TestMovement : MonoBehaviour
 {
     // Start is called before the first frame update
-
+    public enum PlayerState
+    {
+        NotAttacking,
+        Lunging,
+        Grabbing,
+        Dashing
+    }
 
     private Vampire vampireControls;
+    PlayerState playerState;
     Vector2 playerMove;
     bool hasEnemyGrabbed;
+    [SerializeField]bool canAttack = true;
     public float playerSpeed;
+    public float playerLunge;
+    public float playerGrab;
+    public float grabDistance;
+    public float lungeDistance;
     public float rotationSpeed;
+    public float playerDashback;
     Rigidbody2D rb;
 
 
@@ -41,11 +54,15 @@ public class TestMovement : MonoBehaviour
     }
 
     // Update is called once per frame
-    void Update()
+    void FixedUpdate()
     {
-        playerMove = vampireControls.Player.Move.ReadValue<Vector2>();
-        rb.AddRelativeForce(Vector2.up * (playerMove.y * playerSpeed));
-        Rotate();
+        if(playerState == PlayerState.NotAttacking)
+        {
+            playerMove = vampireControls.Player.Move.ReadValue<Vector2>();
+            rb.AddRelativeForce(Vector2.up * (playerMove.y * playerSpeed));
+            Rotate();
+        }
+        
     }
     void Rotate()
     {
@@ -54,25 +71,77 @@ public class TestMovement : MonoBehaviour
 
     public void Button(InputAction.CallbackContext obj)
     {
-        if(!hasEnemyGrabbed) //if currently not grabbing an enemy
+        if(canAttack)
         {
-            if(playerMove.y > 0) // if moving forward lunge attack
+            if (!hasEnemyGrabbed) //if currently not grabbing an enemy
             {
-                Debug.Log("lunge attack");
+                if (playerMove.y > 0) // if moving forward lunge attack
+                {
+                    playerState = PlayerState.Lunging;
+                    Debug.Log("lunge attack");
+                    RaycastHit2D hit = Physics2D.Raycast(transform.position, Vector2.up, lungeDistance);
+                    if(hit.transform.tag == "Enemy")
+                    {
+                        hasEnemyGrabbed = true;
+                    }
+                    rb.AddRelativeForce(Vector2.up * playerLunge);
+                    StartCoroutine(DisableMovement(0.2f));
+                }
+                else if (playerMove.y < 0) // if moving backward dashback
+                {
+                    Debug.Log("Dashback");
+                    playerState = PlayerState.Dashing;
+                    rb.AddRelativeForce(Vector2.down * playerDashback);
+                    StartCoroutine(DisableMovement(0.1f));
+                }
+                else //else do simple grab
+                {
+                    Debug.Log("Grab");
+                    RaycastHit2D hit = Physics2D.Raycast(transform.position, transform.up, grabDistance);
+                    if (hit.transform.tag == "Enemy")
+                    {
+                        hasEnemyGrabbed = true;
+                    }
+                    playerState = PlayerState.Grabbing;
+                    rb.AddRelativeForce(Vector2.up * playerGrab);
+                    StartCoroutine(DisableMovement(0.3f));
+                }
             }
-            else if(playerMove.y < 0) // if moving backward dashback
+            else
             {
-                Debug.Log("Dashback");
+                Debug.Log("keep hold of enemy attempt");
             }
-            else //else do simple grab
-            {
-                Debug.Log("Grab");
-            }
+
         }
-        else
-        {
-            Debug.Log("keep hold of enemy attempt");
-        }
+        
     }
     
+    IEnumerator DisableMovement(float timer)
+    {
+        yield return new WaitForSeconds(timer);
+        playerState = PlayerState.NotAttacking;
+        StartCoroutine(CooldownAttack());
+    }
+
+    IEnumerator CooldownAttack()
+    {
+        float t = 0;
+        while(t < 2f)
+        {
+            t += Time.deltaTime;
+            canAttack = false;
+            yield return null;
+        }
+
+        canAttack = true;
+    }
+
+    private void OnDrawGizmos()
+    {
+        Gizmos.color = Color.yellow;
+        Gizmos.DrawRay(transform.position, transform.up * lungeDistance);
+        Gizmos.color = Color.red;
+        Gizmos.DrawRay(transform.position, transform.up * grabDistance);
+    }
+
 }
